22649527cae4655796b4d2be253f6281ac59a33f
DawudAG <dawudghani@gmail.com>
2024-06-11 22:21:34 +0100

    Added linting

--- app/database.py
+++ app/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = (f"postgresql://{settings.database_username}"
+                           f":{settings.database_password}@{settings.database_hostname}"
+                           f":{settings.database_port}/{settings.database_name}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -11,9 +13,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import posts, users, auth
-from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -12,6 +11,7 @@ app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 
+
 @app.get("/")
 def root():
-  return {"message": "Hello world"}
+    return {"message": "Hello world"}
